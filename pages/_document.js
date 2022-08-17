b87503e46a67501675acefa890ad0d4df353ea3f
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2022-08-17 10:04:12 -0400

    feat: _document.js with portal

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+import {
+  Html,
+  Head, 
+  Main, 
+  NextScript
+} from 'next/document';
+
+const _document = () => {
+  return (
+    <Html lang='en-us'>
+      <Head>
+        <body>
+          <Main />
+          <div id="portal"/>
+          <NextScript />
+        </body>
+      </Head>
+    </Html>
+  )
+}
+
+export default _document;
